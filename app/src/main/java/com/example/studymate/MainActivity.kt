b3053c2c3a9374e79e9508180999ab2ff858c7dd
A <666av6@gmail.com>
2025-12-12 05:39:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/studymate/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.studymate
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.studymate.ui.theme.AppTheme
+import com.example.studymate.ui.theme.MainTheme
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+
+		}
+	}
+}
+
+@Composable
+fun WebViewScreen(
+	modifier: Modifier = Modifier,
+	url: String,
+) {
+	var webView by remember { mutableStateOf<WebView?>(null) }
+
+	BackHandler(enabled = true) {
+		webView?.let { wv ->
+			if (wv.canGoBack()) {
+				wv.goBack()
+			}
+		}
+	}
+
+	AndroidView(
+		modifier = modifier,
+		factory = { context ->
+			WebView(context).apply {
+				layoutParams = ViewGroup.LayoutParams(
+					ViewGroup.LayoutParams.MATCH_PARENT,
+					ViewGroup.LayoutParams.MATCH_PARENT
+				)
+				settings.apply {
+					javaScriptEnabled = true
+					domStorageEnabled = true
+					cacheMode = WebSettings.LOAD_DEFAULT
+					mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+				}
+				webViewClient = object : WebViewClient() {
+					override fun onReceivedError(
+						view: WebView?,
+						request: WebResourceRequest?,
+						error: WebResourceError?
+					) {
+						super.onReceivedError(view, request, error)
+						android.util.Log.e("WebView", "Error: ${error?.description}")
+					}
+				}
+				loadUrl(url)
+				webView = this  // Сохраняем ссылку
+			}
+		},
+		update = { webViewInstance ->
+			webView = webViewInstance
+			webViewInstance.loadUrl(url)
+		}
+	)
+}
+
